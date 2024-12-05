825059a85615d3da65af1c7f5bf01acaf18652dc
Darragh Enright <darragh@greyscout.com>
2024-12-05 08:05:47 +0000

    refactor: create test util file with `seedDatabase` and update e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { seedDatabase, SEED_DATA } from './utils';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -12,13 +13,30 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
     await app.init();
+    await seedDatabase();
   });
 
-  it('GET /api should return status `501 Not Implemented`', () => {
-    // FIXME: this implicitly recognises the global `/api` prefix
-    return request(app.getHttpServer())
+  it('GET / should return status `501 Not Implemented`', async () => {
+    // route automatically uses the global `/api` prefix
+    await request(app.getHttpServer())
       .get('/')
       .expect(HttpStatus.NOT_IMPLEMENTED);
+  });
+
+  it('GET /api/data should return a list of data`', async () => {
+    // route automatically uses the global `/api` prefix
+    const expectedLength = SEED_DATA.length;
+    const expectedBody = JSON.parse(JSON.stringify(SEED_DATA));
+
+    await request(app.getHttpServer())
+      .get('/data')
+      .expect(HttpStatus.OK)
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect((res) => {
+        expect(res.body).toHaveLength(expectedLength);
+        expect(res.body).toEqual(expectedBody);
+      });
   });
 });
