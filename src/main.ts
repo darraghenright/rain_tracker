d53c9b6f548d9d71566804664953f0539df2c954
Darragh Enright <darraghenright@gmail.com>
2024-12-07 01:06:56 +0000

    add `nestjs/swagger` and init API documentation (#5)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+
+function setupApiDocumentation(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Rain Tracker API')
+    .setDescription('Report and view daily rain data')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  // serve docs and JSON endpoints at / and /json respectively
+  SwaggerModule.setup('/', app, documentFactory, {
+    jsonDocumentUrl: 'json',
+  });
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupApiDocumentation(app);
+
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +31,8 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
